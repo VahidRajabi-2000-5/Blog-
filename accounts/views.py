00032c4cfa11f1042295b243f322cf4d6a836621
vahid rajabi <vahidrajabi.software@gmail.com>
2025-02-25 00:00:06 +0330

    Authentication

---
+++ accounts/views.py
@@ -0,0 +1,25 @@
+from django.views import generic
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.urls import reverse_lazy
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy("login")
+
+
+class ProfileEditView(generic.UpdateView):
+    model = User
+    fields = [
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+    ]
+    template_name = "registration/profile_change.html"
+    success_url = reverse_lazy("profile_edit")
+
+    def get_object(self, queryset=None):
+        return self.request.user
--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
-    
+    "accounts",
     # crispy forms
     "crispy_forms",
     "crispy_bootstrap5",
@@ -128,5 +128,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"  
-CRISPY_TEMPLATE_PACK = "bootstrap5" 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LOGIN_REDIRECT_URL = 'home_page'
+LOGOUT_REDIRECT_URL = 'home_page'
